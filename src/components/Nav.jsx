45b99034dcab6db42661bbe9802026339e871c83
VinayKalirawna <vinaykumar7654321@gmail.com>
2025-08-27 20:30:13 +0530

    final change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,46 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <div className="nav">
             <div className="logo">
-                <h1 className="logo">PixelAI</h1>
+                <NavLink to="/" className="logo-link" onClick={closeMenu}>
+                    <h1>PixelAI</h1>
+                </NavLink>
             </div>
 
-            <div className="nav-links-container">
-                <NavLink className="nav-links" to="/">
+            {/* Hamburger Menu Icon */}
+            <div className="hamburger-menu" onClick={toggleMenu}>
+                <span className={`bar ${isMenuOpen ? "open" : ""}`}></span>
+                <span className={`bar ${isMenuOpen ? "open" : ""}`}></span>
+                <span className={`bar ${isMenuOpen ? "open" : ""}`}></span>
+            </div>
+
+            {/* Navigation Links */}
+            <div className={`nav-links-container ${isMenuOpen ? "open" : ""}`}>
+                <NavLink className="nav-links" to="/" onClick={closeMenu}>
                     Home
                 </NavLink>
-                <NavLink className="nav-links" to="/gallery">
+                <NavLink
+                    className="nav-links"
+                    to="/gallery"
+                    onClick={closeMenu}
+                >
                     Gallery
                 </NavLink>
-                <NavLink className="nav-links" to="/about">
+                <NavLink className="nav-links" to="/about" onClick={closeMenu}>
                     About
                 </NavLink>
             </div>
